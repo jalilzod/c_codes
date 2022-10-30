26ba7b8bee03adbc36694797b766b3720024a048
jalilzod <mingyuan_12@qq.com>
2022-10-30 15:11:00 +0800

    sum_of_two_numbers.c

---
+++ sum_of_two_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    int a = 0;
+    int b = 0;
+    int sum = 0;
+    printf("Enter two positive number\n");
+    scanf("%d%d",&a,&b);
+    sum = (a+b);
+    printf("Sum = %d\n ",sum);
+    return 0;
+
+}
