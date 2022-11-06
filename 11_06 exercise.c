20bddb5a248e8f06b632bf8f4a85e95207e8f4eb
jalilzod <mingyuan_12@qq.com>
2022-11-06 16:03:28 +0800

    11_02 homework.exe

---
+++ 11_06 exercise.c
@@ -0,0 +1,94 @@
+//#include <stdio.h>
+//
+//int main ()
+//
+//{
+//    printf("WARNING ALERT\a\a\n");
+//
+//    return 0;
+//}
+#include <stdio.h>
+
+int main ()
+
+//{
+//    printf("hello\nhi\nbye\n");
+//    return 0;
+//}
+
+//{
+//    int a , b;
+//    a = 100;
+//    b = a + 100;
+//    printf("a is %d\nb is %d\n",a,b);
+//    return 0;}
+//{
+//    int a = 0;
+//    int b = a + 2;
+//    scanf("%d",&a);
+//    printf("%d\n%d\n",a,b=(a+2));
+//    return 0;
+//}
+
+//{
+//    int a = 5.4;
+//    int b = a + 2.4;
+//    printf("a=%d\nb=%d\n",a,b);
+//    return 0;
+//}
+//
+//{
+//    int n;
+//    scanf("%d",&n);
+//
+//    printf("%d\n",5*n);
+//    return 0;
+//}
+
+//{
+//    printf("ABCD\n");
+//    puts("ABCD");
+//    return 0;
+//}
+
+//{
+//    int a,b;
+//    int ab;
+//    puts("Enter two number");
+//
+//    printf("Number 1:");  scanf("%d",&a);
+//    printf("Number 2:");  scanf("%d",&b);
+//
+//
+//
+//    ab = a + b;
+//
+//    printf("Tottal is:%d\n",ab);
+//    return 0;
+//}
+
+//{
+//    puts("tian");
+//    puts("di");
+//    puts("ren");
+//    return 0;
+//}
+
+
+//{
+//    int a,b;
+//    scanf("%d%d",&a,&b);
+//
+//    printf("%d\n",a*b);
+//    return 0;
+//}
+
+
+{
+    int length,width,area;
+
+    scanf("%d%d",&length,&width);
+    area = length*width;
+    printf("area :%d\n",area);
+    return 0;
+}
