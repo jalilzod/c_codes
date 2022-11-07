37e066150fcf1f97132771aedc2e983277e706db
jalilzod <mingyuan_12@qq.com>
2022-11-07 16:48:30 +0800

    recurrent.c

---
+++ recurrent.c
@@ -0,0 +1,35 @@
+//#include <stdio.h>
+//
+//int Fun(int n)
+//{
+//    if(n == 5)
+//        return 2;
+//    else
+//        return 2 * Fun(n + 1);
+//}
+//int main()
+//{
+//    int ret = Fun(2);
+//    printf("%d\n",ret);
+//
+//    return 0;
+//}
+
+
+
+#include <stdio.h>
+int Fun(int n)
+{
+    if (n == 5)
+        return 2;
+    else
+        return 2 * Fun(n + 1);
+}
+
+int main ()
+
+{
+    int ret = Fun(2);
+    printf("%d\n",ret);
+    return 0;
+}
