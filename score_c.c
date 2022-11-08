7a4d96cbd8d2c10596db4dd25ce6dc41aed66c67
jalilzod <mingyuan_12@qq.com>
2022-11-08 16:11:09 +0800

    score_c.c

---
+++ score_c.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+    {
+        int No = 0;
+        double a,b,c = 0.0;
+        scanf("%d%lf%lf%lf",&No,&a,&b,&c);
+
+        printf("The each subject score of No %d is %.2lf %.2lf %.2lf ",No,a,b,c);
+
+        return 0;
+    }
