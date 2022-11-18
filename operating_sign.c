090d07832bc42e4b9da633079119215e99002b58
jalilzod <mingyuan_12@qq.com>
2022-11-18 21:43:15 +0800

    operating_sign.c

---
+++ operating_sign.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+//{
+//	double a = 10 % 3;
+//	double b = 10 / 3;
+//	printf("%.2lf\n%.2lf\n", a,b);
+//
+//	return 0;
+//
+//
+//}
+
+//{
+//	int ret = 10 % 3;
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+{
+	// >>youyi caozuofu << zuoyicazuofu caozuofu
+
+	//源码
+	//反码
+	//补码
+	/*int a = 10;
+	int b = a << 1;
+	printf("%d\n%d\n",a,b);
+	return 0;*/
+	int a = -10;
+	int b = a << 1;
+	printf("%d\n", b);
+	return 0;
+}
