80f0644e3a253b94f2491db33bfc8857fa96da30
jalilzod <mingyuan_12@qq.com>
2022-11-29 22:40:30 +0800

    days_of_week.c

---
+++ days_of_week.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int day = 0;
+	scanf("%d", &day);
+
+	int n = 2;
+	switch (day)
+	{
+	case 1: printf("Monday\n"); break;
+	case 2: printf("Tuesday\n"); break;
+	case 3: printf("Wednsday\n"); break;
+	case 4: printf("Thursday\n"); break;
+	case 5: printf("Friday\n"); break;
+	case 6: printf("Saturday\n"); break;
+	case 7: printf("Sunday\n"); break;
+
+	}
+	return 0;
+}
