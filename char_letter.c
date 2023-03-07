de49555f25e9535bfcd4f5238493edf139c1b83b
jalilzod <mingyuan_12@qq.com>
2023-03-07 10:00:37 +0800

    char_letter.c

---
+++ char_letter.c
@@ -0,0 +1,16 @@
+int main(void)
+{
+	int i,j,k;
+	for (i = 48; i <= 57; i++) {
+		printf("%c\n", i);
+	}
+
+	for (j = 65; j <= 90; j++) {
+		printf("%c ", j);
+	}
+
+	for (k = 97; k <= 122; k++) {
+		printf("%c ", k);
+	}
+	return 0;
+}
